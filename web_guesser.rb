395f2321d9ee21d8aed089cb763cd4e9eb4b8aba
Kit Langton <kitlangton@gmail.com>
2015-12-15 19:32:31 -0500

    I5: Implement remaining gueses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,34 +1,62 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :secret_number, rand(101)
+@@secret_number = rand(101)
+@@remaining_guesses = 5
+@@should_reset = false
 
 get '/' do
 
+  reset_game if @@should_reset
+  @@should_reset = false
+
   guess = params["guess"]
+  cheatmode = params["cheat"] == 'true'
 
   message, status = check_guess(guess)
+  unless status == :empty
+    @@remaining_guesses -= 1
+  end
 
-  erb :index, locals: { secret_number: settings.secret_number, message: message, status: status}
+  if @@remaining_guesses == 0
+    @@should_reset = true
+    @@remaining_guesses = 5
+    message = "You're out of guesses! Resetting the number and starting again"
+  end
+
+  if status == :correct
+    @@should_reset = true
+    @@remaining_guesses = 5
+    message = "You got it! Resetting the number and starting again"
+  end
+
+
+  erb :index, locals: { secret_number: @@secret_number, message: message, status: status, cheatmode: cheatmode}
+
+end
+
+def reset_game
+    @@secret_number = rand(101)
+    @@remaining_guesses = 5
 end
 
 def check_guess(guess)
   return ["Enter a guess!", :empty] if guess == ""
   guess = guess.to_i
 
-  if guess - 5 > settings.secret_number
+  if guess - 5 > @@secret_number
     message = "Way too high!"
     value = :wayhigh
-  elsif guess > settings.secret_number
+  elsif guess > @@secret_number
     message ="Too high!"
     value = :high
-  elsif guess + 5 < settings.secret_number
+  elsif guess + 5 < @@secret_number
     message ="Way too low!"
     value = :waylow
-  elsif guess < settings.secret_number
+  elsif guess < @@secret_number
     message ="Too low!"
     value = :low
-  elsif guess == settings.secret_number
+  elsif guess == @@secret_number
     message = "You got it!"
     value = :correct
   end
